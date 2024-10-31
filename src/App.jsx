af19ec08f7b875a8bc6d42c931aa260a5204bf81
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-31 19:13:08 +0530

    basic structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { Button } from 'react-bootstrap';
+import { Card, Col, Form, Row } from 'react-bootstrap';
 
 
 
 export default function App() {
   return (
     <>
+      <Row style={{justifyContent:'center'}}>
+        <Col xs={8} className="mt-5">
+          <Card className="p-4">
+            <h1 className='mb-4'>Sign up</h1>
+            <RegistrationForm />
+          </Card>
+        </Col>
+      </Row>
     </>
+  );
+}
+
+function RegistrationForm(){
+  return (
+    <Form>
+
+    </Form>
   );
 }
